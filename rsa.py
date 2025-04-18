44f9815249dce8e45e8026157d6d382fcd482ab3
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2025-04-18 14:36:52 +0300

    Implement key generation in `generate_keypair()`

--- rsa.py
+++ rsa.py
@@ -1,5 +1,27 @@
-def generate_keypair():
-    return ()
+import random
+import utils
+
+def generate_keypair(length):
+    if length <= 1:
+        raise ValueError("Key length too small")
+
+    p = utils.generate_prime(length)
+    q = utils.generate_prime(length)
+    while q == p:
+        q = utils.generate_prime(length)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    while True:
+        e = random.randrange(3, phi, 2)
+        if utils.modexp(e, 2, phi) == 1:
+            # skip if the inverse of e mod phi is e
+            continue
+        if utils.gcd(e, phi) == 1:
+            break
+
+    d = utils.modinv(e, phi)
+    return (e, n), (d, n)
 
 def encrypt(message, public_key):
     return ""
